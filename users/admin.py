c479a83767fe30cb3118c1b3f4ef511b1a517933
Екатерина Кудряшова <katyushka555@yandex.ru>
2024-06-24 13:57:32 +0400

    Добавление функции на оплату конкретного продукта

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,10 @@ from users.models import Payments, User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("id", "email",)
+    list_display = (
+        "id",
+        "email",
+    )
 
 
 @admin.register(Payments)
--- users/services.py
+++ users/services.py
@@ -4,13 +4,24 @@ from config.settings import STRIPE_API_KEY
 stripe.api_key = STRIPE_API_KEY
 
 
-def create_stripe_price(sum_payment):
+def create_stripe_product(product):
+    """Создает продукт на оплату."""
+
+    name_product = (f"{product.paid_course}" if product.paid_course else f"{product.paid_lesson}")
+    stripe_product = stripe.Product.create(name=f"{name_product}")
+    return stripe_product.get("id")
+
+
+def create_stripe_price(sum_payment, stripe_product_id):
     """Создает цену в страйпе."""
+
+    # print('sum_payment', sum_payment) # Что передается в 'sum_payment'
 
     return stripe.Price.create(
         currency="rub",
-        unit_amount=int(sum_payment * 100),
-        product_data={"name": "Payments"},
+        unit_amount=sum_payment * 100,
+        # product_data={"name": "Payments"},
+        product=stripe_product_id,
     )
 
 
